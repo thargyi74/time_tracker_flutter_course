0c12dbac5b446a60a17eb0bb051b91d70cdbd765
Ye Min Naing <thargyi74@gmail.com>
2019-04-26 11:38:28 +0630

    after deleting initState _checkCurrentUser and _updatedUser

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -4,53 +4,25 @@ import 'package:time_tracker_flutter_course/app/sign_in_page/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth_base.dart';
 import 'package:time_tracker_flutter_course/services/user.dart';
 
-class LandingPage extends StatefulWidget {
+class LandingPage extends StatelessWidget {
   LandingPage({@required this.auth});
 
   final AuthBase auth;
-  @override
-  _LandingPageState createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-  User _user;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _checkCurrentUser();
-    widget.auth.onAuthStateChanged.listen((user) {
-      print('User: ${user?.uid}');
-    });
-  }
-
-  Future<void> _checkCurrentUser() async {
-    User user = await widget.auth.currentUser();
-    _updatedUser(user);
-  }
-
-  void _updatedUser(User user) {
-    setState(() {
-      _user = user;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: widget.auth.onAuthStateChanged,
+    return StreamBuilder<User>(
+      stream: auth.onAuthStateChanged,
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
+        if (snapshot.connectionState == ConnectionState.active) {
           User user = snapshot.data;
           if (user == null) {
             return SignInPage(
-              auth: widget.auth,
-              onSignIn: _updatedUser,
+              auth: auth,
             );
           } else {
             return HomePage(
-              auth: widget.auth,
-              onSignOut: () => _updatedUser(null),
+              auth: auth,
             ); // to do HomePage
           }
         } else {
