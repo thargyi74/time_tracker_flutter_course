9eb8b836009192a8ca72802b86ac93e8f3c1f1f9
Ye Min Naing <thargyi74@gmail.com>
2019-07-02 12:35:48 +0630

    Estimated total weight of wast (Kg)

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -6,13 +6,67 @@ import 'package:time_tracker_flutter_course/common_widgets/platform_alert_dialog
 import 'package:time_tracker_flutter_course/services/auth_base.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  static const LatLng _center = const LatLng(16.871311, 96.199379);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static const LatLng _center = const LatLng(16.871311, 96.199379);
+  final Set<Marker> _markers = {};
+
+  //LatLng _lastMapPosition = HomePage._center;
+  LatLng _iconPosition = LatLng(16.871301, 96.199369);
+
+  DateTime selectedDate = DateTime.now();
+  TimeOfDay selectedTime = TimeOfDay.now();
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
+  }
+
+  void _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+        // This marker id can be anything that uniquely identifies each marker.
+        markerId: MarkerId(_iconPosition.toString()),
+        position: _iconPosition,
+        infoWindow: InfoWindow(
+          title: 'Really cool place',
+          snippet: '5 Star Rating',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2019, 1),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (picked != null && picked != selectedTime) {
+      setState(() {
+        selectedTime = picked;
+      });
+    }
   }
 
   @override
@@ -50,25 +104,64 @@ class HomePage extends StatelessWidget {
               child: Text(
                 'Search and pin location',
                 style: TextStyle(
-                    color: Colors.indigo, fontSize: 15, letterSpacing: 1.0),
+                    color: Colors.black, fontSize: 15, letterSpacing: 0.5),
                 textAlign: TextAlign.center,
               ),
             ),
           ),
-          Center(
-            child: Container(
-              width: 400,
-              height: 350,
-              padding: EdgeInsets.all(20.0),
-              child: GoogleMap(
-                onMapCreated: _onMapCreated,
-                initialCameraPosition: CameraPosition(
-                  target: _center,
-                  zoom: 11.0,
+          Stack(
+            children: <Widget>[
+              Container(
+                width: 450,
+                height: 350,
+                padding: EdgeInsets.all(8.0),
+                child: GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  markers: _markers,
+                  initialCameraPosition: CameraPosition(
+                    target: HomePage._center,
+                    zoom: 11.0,
+                  ),
                 ),
               ),
-            ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: FloatingActionButton(
+                    onPressed: _onAddMarkerButtonPressed,
+                    materialTapTargetSize: MaterialTapTargetSize.padded,
+                    backgroundColor: Colors.green,
+                    child: const Icon(Icons.add_location, size: 32.0),
+                  ),
+                ),
+              ),
+            ],
           ),
+          Text('Date & Time'),
+          Row(
+            children: <Widget>[
+              RaisedButton.icon(
+                  onPressed: () {
+                    _selectDate(context);
+                  },
+                  icon: Icon(Icons.date_range),
+                  label: Text('Select Date')),
+              SizedBox(
+                width: 8.0,
+              ),
+              RaisedButton.icon(
+                  onPressed: () {
+                    _selectTime(context);
+                  },
+                  icon: Icon(Icons.access_time),
+                  label: Text('Select Time')),
+            ],
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text('Estimated total weight of wasts (Kg)'),
         ],
       ),
     );
